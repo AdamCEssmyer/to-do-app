1ed47177d4e5ccf403830a88b4adb3e3de3a256b
Adam Essmyer <acemyer@Adams-MacBook-Pro.local>
2018-02-12 17:32:44 -0600

    completed 7 steps from exercise

--- app.js
+++ app.js
@@ -1,50 +1,73 @@
 function onReady() {
+
+  let id = 0;
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
-  });
-}
+    function createNewToDo() {
+
+        if(!newToDoText.value) { return; }
+
+        toDos.push({
+          title: newToDoText.value,
+          complete: false
+          id: 0++;
 
 
 
-    //get the text
-    let title = newToDoText.value;
 
-    //create new li
-    let newLi = document.createElement('li');
+        });
 
-    //create new input
-    let checkbox = document.createElement('input');
+        newToDoText.value = '';
 
-    // set input's type to checkbox
-    checkbox.type = "checkbox";
-
-    //set title
-    newLi.textContent = title;
-
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    //attach li to ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
-    newToDoText.value = '';
-
-    //delete task
-    var deleteTask = function() {
-      console.log("Delete task...");
-      var listItem = this.parentNode;
-      var ul = listItem.parentNode;
-      ul.removeChild(listItem);
+        renderTheUI();
     }
 
+    //renderTheUI
+      function renderTheUI(){
+          //delete button
+          object.addEventListener("click", newLi);
+
+        const toDoList = document.getElementById('toDoList');
+
+        toDoList.textContent = '';
+
+        toDos.forEach(function(toDo) {
+          const newToDo = document.createElement('li');
+
+          const checkbox = document.createElement('input');
+          checkbox.type = "checkbox";
+
+          const title = document.createElement('span');
+          title.textContent = toDo.title;
+
+          toDoList.appendChild(newLi);
+          newLi.appendChild(checkbox);
+        });
+        
+          //Delete ToDo function
+          function deleteToDo(id){
+            if(toDos = toDos.filter(item => item.id ! == id)){
+            return true;
+          } else {
+            return false;
+          }
+        }
+
+      }
+
+      //Event Listener
+      addToDoForm.addEventListener('submit', event =>{
+        event.preventDefault();
+          deleteToDo(toDo.id);
+          createNewToDo();
+          renderTheUI();
+
+
+      });
 
 window.onload = function() {
-  alert("The window has loaded!");
   onReady();
-  }
-};
+}
